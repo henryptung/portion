2d4f1924c152b6d21160d899d8d4aab9862de343
Henry Tung <henryptung@gmail.com>
2026-06-13 00:50:43 -0700

    Add performance benchmarks for #106 and #108

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -323,6 +323,12 @@ class TestIntervalOverlaps():
         with pytest.raises(TypeError):
             P.closed(0, 1).overlaps(1)
 
+    @pytest.mark.parametrize("size", [10, 100, 1000, 10000])
+    def test_overlaps_benchmark(self, benchmark, size):
+        domain = P.Interval(*(P.closed(2*i, 2*i+1) for i in range(size)))
+        other = P.closed(size / 3, 2 * size / 3)
+        benchmark(lambda: domain.overlaps(other))
+
 
 class TestIntervalComparison:
     @pytest.mark.parametrize('i1,i2,i3', [
